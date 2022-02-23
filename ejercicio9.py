d07aa9ed1c8067194cc2566905a45ed64715d331
Andrea Casas <91721826+acasasaez@users.noreply.github.com>
2022-02-23 15:46:06 +0000

    9

--- ejercicio9.py
+++ ejercicio9.py
@@ -19,3 +19,29 @@ variable
 inicialización
     i ← I_MIN
     Resultado = AUSENTE # Todavía no se ha encontrado     nada
+realización
+    mientras que
+        i ≤ I_MAX et Resultado = AUSENTE
+        invariante
+            (∀k ∈ Z)(I_MIN ≤ k < i => Resultado = AUSENTE) <=> (∀k ∈ Z)
+            (I_MIN ≤ k < i => ítem(diccionario[k], 1) ≠ inicial
+        variante de control
+            I_MAX – i + 1
+    repetir
+        si
+            ítem(diccionario[i], 1) = inicial
+        entonces
+            afirmación
+                i ≤ I_MAX
+                (∀k ∈ Z)(I_MIN ≤ k < i => Resultado = AUSENTE) et
+                ítem(diccionario[i], 1) = inicial
+            Resultado ← i
+            afirmación
+                i ≤ I_MAX
+                Resultado = i ≠ AUSENTE
+        fin si
+        i ← i + 1
+        afirmación
+            i ≤ I_MAX + 1
+            Resultado = i o Resultado = AUSENTE
+    fin repetir
