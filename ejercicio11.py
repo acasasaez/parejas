e3fcaaf10804a6a5d57866b3f7765055b9509864
acasasaez <acasasae@myuax.com>
2022-02-23 12:44:13 +0100

    .

--- ejercicio11.py
+++ ejercicio11.py
@@ -5,5 +5,22 @@ precondición
     n ≥ 0 ; m ≥ 0
     # 0 no tiene máximo divisor
     n ≠ 0 o si no m ≠ 0
+variable
+    dividendo, divisor, resto : ENTERO
+inicialización
+    divisor ← n
+    resto ← m
+realización
+    mientras que
+        resto > 0
+        invariante
+            (H): la última división de la serie ha utilizado el
+            resto de la división anterior como divisor y
+            ha dado resto como resultado.
+
+        variante de control
+            resto
+    repetir
+
 
 
