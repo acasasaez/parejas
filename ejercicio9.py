f17918aa6d9313d05e72ddcee2e8406209719345
Andrea Casas <91721826+acasasaez@users.noreply.github.com>
2022-02-23 15:27:29 +0000

    ej 9

---
+++ ejercicio9.py
@@ -0,0 +1,18 @@
+Algoritmo índice_palabra
+    # La primera palabra de `diccionario' que empieza por
+    # `inicial'.
+Entrada
+    inicial : CARACTER
+        # La inicial a buscar
+    diccionario : TABLA[CADENA]
+        # Objetivo de la búsqueda
+Resultado : ENTERO
+precondición
+    es_alfabético(inicial)
+    está_definido(diccionario)
+constante
+    I_MIN : ENTERO ← índice_min(diccionario)
+    I_MAX : ENTERO ← índice_max(diccionario)
+    AUSENTE : ENTERO ← ???
+variable
+    i : ENTERO # Índice siguiente celda a observar
