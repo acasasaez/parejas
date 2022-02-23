72c69efbbee22390cd070fada728c633ca3e9659
Andrea Casas <91721826+acasasaez@users.noreply.github.com>
2022-02-23 17:03:33 +0000

    postcondicion

--- ejercicio12.py
+++ ejercicio12.py
@@ -7,3 +7,22 @@ Entrada
   
 Precondicion
   n>=0
+
+Postcondición
+  #El número n no se ha modificado 
+  antiguo(n)= n
+  #Las celdas de la tabla inicial no se han modificado 
+  antiguo(
+        sub_tabla(cuadrados,
+                 antiguo(indice_min(cuadrados))+1,
+                 antiguo(cuadrados[indice_min(cuadrados)])
+                ) = sub_tabla(cuadrados,
+                 antiguo(indice_min(cuadrados))+1,
+                 antiguo(cuadrados[indice_min(cuadrados)])
+  #La tabla se amplía si es insuficiente 
+    cuadrados[antiguo(indice_min(cuadrados))] < n => cuadrados[indice_min(cuadrados)] >= n
+  #En caso contrario no se modifica ni la tabla ni indice_min(cuadrados)
+     cuadrados[antiguo(indice_min(cuadrados))] => antiguo(indice_min(cuadrados)) =     indice_min(cuadrados)
+  #`raiz`es la raiz cuadrada de `n`
+  raiz**2 <= n< (raiz + 1)**2 <=> raiz = √n
+fin raiz_cuadrada_entera
