eb70b0bc45c285b42617291cc1b0190b08ec1c05
Andrea Casas <91721826+acasasaez@users.noreply.github.com>
2022-02-23 14:10:55 +0000

    continuación

--- ejercicio8.py
+++ ejercicio8.py
@@ -29,3 +29,20 @@ mientras que
         (H)
     variante de control
         L - k
+repetir
+    afirmación
+        ítem(ca, k) = separador y ítem(ca, r) = separador
+    # Se copia la cadena `ca' entre los caracteres
+    # de índices k+1 y r-1
+    componentes[i] ← sub_cadena(ca, k+1, r-1)
+    dimensión ← i
+    afirmación
+        componer(componentes, 1, i, separador) = sub_cadena(ca, 1, r–1)
+            # componentes[1..i] contiene descomposición de ca[1..r-1]
+        dimensión = i
+        ítem(ca, k) = separador
+            # es la penúltima ocurrencia de `separador'
+        ítem(ca, r) = separador
+            # es la última ocurrencia de `separador'
+    #Ajusta la cantidad de componentes
+    i ← i+1
