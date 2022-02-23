a519d28d755b599eb7a094a513c341cbd6231d3b
acasasaez <acasasae@myuax.com>
2022-02-23 12:33:08 +0100

    Update ejercicio11.py

---
+++ ejercicio11.py
@@ -0,0 +1,9 @@
+mcd(n, m : ENTERO) : ENTERO
+    # El Máximo Común Divisor de `n' y `m'.
+precondición
+    # Enteros naturales
+    n ≥ 0 ; m ≥ 0
+    # 0 no tiene máximo divisor
+    n ≠ 0 o si no m ≠ 0
+
+
