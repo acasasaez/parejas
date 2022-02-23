c556233834e1a7be230fb2dd11cf8e1a7a5cde90
Andrea Casas <91721826+acasasaez@users.noreply.github.com>
2022-02-23 16:41:41 +0000

    ejercicio 12 p2 entrada

--- ejercicio12.py
+++ ejercicio12.py
@@ -1,1 +1,7 @@
-hola
+Algoritmo raiz_cuadrada_entera
+   #Este algoritmo calcula la raíz cuadrada entera de un número entero n 
+Entrada: 
+  n:ENTERO #cálculo de la raíz entera de n entero
+  cuadrados: TABLA [ENTERO] #La tabla de los cuadrados   enteros
+  raíz: ENTERO #entero "raíz cudrada de n"
+  
