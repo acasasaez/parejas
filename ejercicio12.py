e73c524294db0f7ecab74ad143fb85300747709f
Andrea Casas <91721826+acasasaez@users.noreply.github.com>
2022-02-23 16:48:07 +0000

    precondición

--- ejercicio12.py
+++ ejercicio12.py
@@ -1,7 +1,9 @@
 Algoritmo raiz_cuadrada_entera
    #Este algoritmo calcula la raíz cuadrada entera de un número entero n 
-Entrada: 
+Entrada 
   n:ENTERO #cálculo de la raíz entera de n entero
   cuadrados: TABLA [ENTERO] #La tabla de los cuadrados   enteros
   raíz: ENTERO #entero "raíz cudrada de n"
   
+Precondicion
+  n>=0
